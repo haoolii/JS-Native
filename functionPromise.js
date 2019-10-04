71fb5b56cf5231596d4bb6427661ae3322e370fb
unnhao <unnhao@gmail.com>
2019-10-04 16:38:52 +0800

    :bug: promise function fix

--- functionPromise.js
+++ functionPromise.js
@@ -1,49 +1,62 @@
 var myOwnPromise = function(executor) {
   var self = this
+
   self.status = 'pending'
-  self.data = undefined
   self.onResolvedCallback = []
   self.onRejectedCallback = []
 
   function resolve(value) {
-    if (self.status === 'pending') {
-      self.status = 'resolved'
-      self.data = value
-      for (var i = 0; i < self.onResolvedCallback.length; i++) {
-        self.onResolvedCallback[i](value)
-      }
+    if (value instanceof Promise) {
+      return value.then(resolve, reject)
     }
+    setTimeout(function() {
+      if (self.status === 'pending') {
+        self.status = 'resolved'
+        self.data = value
+        for (var i = 0; i < self.onResolvedCallback.length; i++) {
+          self.onResolvedCallback[i](value)
+        }
+      }
+    })
   }
 
   function reject(reason) {
-    if (self.status === 'pending') {
-      self.status = 'rejected'
-      self.data = reason
-      for (var i = 0; i < self.onRejectedCallback.length; i++) {
-        self.onRejectedCallback[i](reason)
+    setTimeout(function() {
+      if (self.status === 'pending') {
+        self.status = 'rejected'
+        self.data = reason
+        for (var i = 0; i < self.onRejectedCallback.length; i++) {
+          self.onRejectedCallback[i](reason)
+        }
       }
-    }
+    })
   }
 
   try {
     executor(resolve, reject)
-  } catch (e) {
-    reject(e)
+  } catch (reason) {
+    reject(reason)
   }
 }
 
 myOwnPromise.prototype.then = function(onResolved, onRejected) {
   var self = this
   var promise2
-  onResolved = typeof onResolved === 'function' ? onResolved : function(v) {
-    return v
-  }
-  onRejected = typeof onRejected === 'function' ? onRejected : function(r) {
-    throw r
-  }
+  onResolved =
+    typeof onResolved === 'function'
+      ? onResolved
+      : function(v) {
+          return v
+        }
+  onRejected =
+    typeof onRejected === 'function'
+      ? onRejected
+      : function(r) {
+          throw r
+        }
 
   if (self.status === 'resolved') {
-    return promise2 = new myOwnPromise(function(resolve, reject) {
+    return (promise2 = new myOwnPromise(function(resolve, reject) {
       setTimeout(function() {
         try {
           var x = onResolved(self.data)
@@ -52,11 +65,11 @@ myOwnPromise.prototype.then = function(onResolved, onRejected) {
           reject(reason)
         }
       })
-    })
+    }))
   }
 
   if (self.status === 'rejected') {
-    return promise2 = new myOwnPromise(function(resolve, reject) {
+    return (promise2 = new myOwnPromise(function(resolve, reject) {
       setTimeout(function() {
         try {
           var x = onRejected(self.data)
@@ -65,11 +78,11 @@ myOwnPromise.prototype.then = function(onResolved, onRejected) {
           reject(reason)
         }
       })
-    })
+    }))
   }
 
   if (self.status === 'pending') {
-    return promise2 = new myOwnPromise(function(resolve, reject) {
+    return (promise2 = new myOwnPromise(function(resolve, reject) {
       self.onResolvedCallback.push(function(value) {
         try {
           var x = onResolved(value)
@@ -80,19 +93,68 @@ myOwnPromise.prototype.then = function(onResolved, onRejected) {
       })
 
       self.onRejectedCallback.push(function(reason) {
-          try {
-            var x = onRejected(reason)
-            resolvePromise(promise2, x, resolve, reject)
-          } catch (r) {
-            reject(r)
-          }
-        })
-    })
+        try {
+          var x = onRejected(reason)
+          resolvePromise(promise2, x, resolve, reject)
+        } catch (r) {
+          reject(r)
+        }
+      })
+    }))
   }
 }
 
 myOwnPromise.prototype.catch = function(onRejected) {
   return this.then(null, onRejected)
+}
+
+function resolvePromise(promise2, x, resolve, reject) {
+  var then
+  var thenCalledOrThrow = false
+
+  if (promise2 === x) {
+    return reject(new TypeError('Chaining cycle detected for promise!'))
+  }
+
+  if (x instanceof Promise) {
+    if (x.status === 'pending') {
+      x.then(function(v) {
+        resolvePromise(promise2, v, resolve, reject)
+      }, reject)
+    } else {
+      x.then(resolve, reject)
+    }
+    return
+  }
+
+  if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
+    try {
+      then = x.then
+      if (typeof then === 'function') {
+        then.call(
+          x,
+          function rs(y) {
+            if (thenCalledOrThrow) return
+            thenCalledOrThrow = true
+            return resolvePromise(promise2, y, resolve, reject)
+          },
+          function rj(r) {
+            if (thenCalledOrThrow) return
+            thenCalledOrThrow = true
+            return reject(r)
+          }
+        )
+      } else {
+        resolve(x)
+      }
+    } catch (e) {
+      if (thenCalledOrThrow) return
+      thenCalledOrThrow = true
+      return reject(e)
+    }
+  } else {
+    resolve(x)
+  }
 }
 
 module.exports = { myOwnPromise }
